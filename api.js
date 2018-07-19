c4577d14b78b85de09a65384c526a083e068dfc1
xcstream <cheng.xu@all-dream.com>
2018-07-19 17:36:39 +0800

    apisave

--- api.js
+++ api.js
@@ -1,9 +1,4 @@
 var fs=require('fs')
 
 app.use('/list',(_,res)=>fs.readdir('public/draw',(err,files)=>res.send(err?{code:500,msg:err}:{code:200,info:files})))
-app.use('/save',(req,res)=>{
-    var name = req.body.name
-    var content = req.body.content
-    fs.writeFileSync('public/draw/'+name, content)
-    res.send({code:200})
-})
+app.use('/save',(req,res)=>res.send(fs.writeFileSync('public/draw/'+req.body.name,req.body.content)||{code:200}))

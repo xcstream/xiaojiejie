f91c2ac04797be03d3275b59c37fa63131f6db46
xcstream <cheng.xu@all-dream.com>
2018-07-19 12:48:09 +0800

    1234

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
 var fs=require('fs')
 
 app.use('/list',(_,res)=>fs.readdir('public/draw',(err,files)=>res.send(err?{code:500,msg:err}:{code:200,info:files})))
+app.use('/save',(req,res)=>{
+    var name = req.body.name
+    var content = req.body.content
+})
